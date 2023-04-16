d055224f08f6283369543f7e11b976a0635e3660
Kirill Moiseev <hi.sender@gmail.com>
2023-04-16 21:44:23 +0200

    gemfile

--- lib/shared_credentials.rb
+++ lib/shared_credentials.rb
@@ -4,5 +4,20 @@ require_relative "shared_credentials/version"
 
 module SharedCredentials
   class Error < StandardError; end
+
   # Your code goes here...
+  def self.deep_transform(hash)
+    return hash unless hash.is_a?(Hash)
+
+    h = ActiveSupport::InheritableOptions.new
+    hash.each do |k, v|
+      h[k] = deep_transform(v)
+    end
+    h
+  end
+
+  def self.env
+    hash = Rails.application.credentials.fetch(Rails.env.to_sym)
+    ActiveSupport::InheritableOptions.new(deep_transform(hash))
+  end
 end
